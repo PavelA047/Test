de0ae0271f9762861ee2c2bd21cf45dc9904de8b
pavel <zzzzfogzzzz@gmail.com>
2021-10-25 01:14:25 +0600

    hw

--- src/main/java/TestPack/TestClass.java
+++ src/main/java/TestPack/TestClass.java
@@ -6,29 +6,5 @@ import java.util.List;
 
 public class TestClass {
 
-    public int[] method1(int[] arr) {
-        Integer[] integerArr = new Integer[arr.length];
-        for (int i = 0; i < arr.length; i++) {
-            integerArr[i] = arr[i];
-        }
-        List<Integer> list = new ArrayList<>(Arrays.asList(integerArr));
-        if (!list.contains(4)) {
-            throw new RuntimeException("there is no 4");
-        }
-        list.subList(0, list.lastIndexOf(4) + 1).clear();
-        int[] newArr = new int[list.size()];
-        for (int i = 0; i < list.size(); i++) {
-            newArr[i] = list.get(i);
-        }
-        return newArr;
-    }
 
-    public boolean method2(int[] arr) {
-        Integer[] integerArr = new Integer[arr.length];
-        for (int i = 0; i < arr.length; i++) {
-            integerArr[i] = arr[i];
-        }
-        List<Integer> list = new ArrayList<>(Arrays.asList(integerArr));
-        return list.contains(4) && list.contains(1);
-    }
 }
